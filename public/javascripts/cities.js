20272df1bc53785de875838aaac7ca4096c1d915
Tatiana <tatianayerma.08@gmail.com>
2023-05-03 19:24:16 +0100

    update

---
+++ public/javascripts/cities.js
@@ -0,0 +1,124 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [2.1734, 41.3851]
+        },
+        "properties": {
+          "cit_id": 1,
+          "cit_name": "Barcelona"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [2.3522, 48.8566]
+        },
+        "properties": {
+          "cit_id": 2,
+          "cit_name": "Paris"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [4.9041, 52.3676]
+        },
+        "properties": {
+          "cit_id": 3,
+          "cit_name": "Amsterdam"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [25.4615, 36.3932]
+        },
+        "properties": {
+          "cit_id": 4,
+          "cit_name": "Santorini"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [18.0944, 42.6507]
+        },
+        "properties": {
+          "cit_id": 5,
+          "cit_name": "Dubrovnik"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [12.5683, 55.6761]
+        },
+        "properties": {
+          "cit_id": 6,
+          "cit_name": "Copenhagen"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-9.2426043, 38.7435977]
+        },
+        "properties": {
+          "cit_id": 7,
+          "cit_name": "Lisbon"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [16.3738, 48.2082]
+        },
+        "properties": {
+          "cit_id": 8,
+          "cit_name": "Vienna"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [18.5065015, 47.4795219]
+        },
+        "properties": {
+          "cit_id": 9,
+          "cit_name": "Budapest"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [17.8172485, 59.326038]
+        },
+        "properties": {
+          "cit_id": 10,
+          "cit_name": "Stockholm"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [11.2300171, 48.1544891]
+        },
+        "properties": {
+          "cit_id": 11,
+          "cit_name": "Munich"
+        }
+      },]}
--- public/javascripts/script_maps.js
+++ public/javascripts/script_maps.js
@@ -4,10 +4,127 @@ let directionsRenderer;
 
 function initMap() {
   // criar um objeto de mapa
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 38.7435977, lng: -9.2426043 }, // coordenadas do centro do mapa
-                zoom: 11, // zoom inicial do mapa
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.7435977, lng: -9.2426043 }, 
+    zoom: 5
   });
+     
+  var cities = [
+    {
+      name: 'Barcelona',
+      lat: 41.3851,
+      lng: 2.1734
+    },
+    {
+      name: 'Paris',
+      lat: 48.8566,
+      lng: 2.3522
+    },
+    {
+      name: 'Amsterdam',
+      lat: 52.3676,
+      lng: 4.9041
+    },
+    {
+      name: 'Santorini',
+      lat: 36.3932,
+      lng: 25.4615
+    },
+    {
+      name: 'Dubrovnik',
+      lat: 42.6507,
+      lng: 18.0944
+    },
+    {
+      name: 'Copenhagen',
+      lat: 55.6761,
+      lng: 12.5683
+    },
+    {
+      name: 'Lisbon',
+      lat: 38.7435977,
+      lng: -9.2426043
+    },
+    {
+      name: 'Vienna',
+      lat: 48.2082,
+      lng: 16.3738
+    },
+    {
+      name: 'Budapest',
+      lat: 47.4795219,
+      lng: 18.5065015
+    },
+    {
+      name: 'Stockholm',
+      lat: 59.326038,
+      lng: 17.8172485
+    },
+    {
+      name: 'Munich',
+      lat: 48.1544891,
+      lng: 11.2300171
+    },
+    {
+      name: 'Edinburgh',
+      lat: 55.9411809,
+      lng: -3.2877387
+    },
+    {
+      name: 'Bruges',
+      lat: 51.2606675,
+      lng: 3.057302
+    },
+    {
+      name: 'Florence',
+      lat: 43.7799286,
+      lng: 11.1585671
+    },
+    {
+      name: 'Prague',
+      lat: 50.059553,
+      lng: 14.3008174
+    },
+    {
+      name: 'Salzburg',
+      lat: 47.8027805,
+      lng: 12.9740295
+    },
+    {
+      name: 'Tallinn',
+      lat: 59.4715893,
+      lng: 24.5734375
+    },
+    {
+      name: 'Reykjavik',
+      lat: 64.1334671,
+      lng: -21.9348418
+    },
+    {
+      name: 'Zurich',
+      lat: 47.3774122,
+      lng: 8.454335
+    },
+    {
+      name: 'Dublin',
+      lat: 53.3242066,
+      lng: -6.4105097
+    },
+    {
+      name: 'Berlin',
+      lat: 52.5063862,
+      lng: 13.095099
+    }
+  ];
+      
+          for (var i = 0; i < cities.length; i++) {
+            var city = cities[i];
+            var marker = new google.maps.Marker({
+              position: {lat: city.lat, lng: city.lng},
+              map: map,
+              title: city.name
+            });
+          }
    // Cria um objeto de mapa com as opções de limites para a Europa
    var map = new google.maps.Map(document.getElementById("map"), {
     center: brussels,
