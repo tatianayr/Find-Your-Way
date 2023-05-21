84204dab13a4c7c2b71a571a3098f80cf56b783b
Camila Madatali <camilabm19@gmail.com>
2023-05-21 14:49:24 +0100

    up

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,12 +1,3 @@
-let profilePic = document.getElementById("profile-pic");
-let inputFile = document.getElementById("input-file");
-
-inputFile.onchange = function () {
-    profilePic.src = URL.createObjectURL(inputFile.files[0]);
-}
-
-
-
 
 window.onload = async function () {
     try {
